5a5f03f159105783c39a20a9f3556a1753dec67d
Osama Almas Rahman <o.almas@hotmail.com>
2023-01-16 03:02:50 +0600

    login wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,44 @@
 import Link from "next/link";
+import { useState } from "react";
 import Layout from "../src/components/Layout";
 
-const IndexPage = () => <h1>Hello Next.js 👋</h1>;
+const IndexPage = () => {
+  let [formFields, setFormFields] = useState({ email: "", password: "" });
+  const login = () => {
+    console.log(formFields);
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          type="email"
+          name="email"
+          value={formFields.email}
+          onChange={(event) => {
+            setFormFields((e) => {
+              e.email = event.target.value;
+              return { ...e };
+            });
+          }}
+        />
+        <input
+          type="password"
+          name="password"
+          value={formFields.password}
+          onChange={(event) => {
+            setFormFields((e) => {
+              e.password = event.target.value;
+              return { ...e };
+            });
+          }}
+        />
+        <button type="button" onClick={login}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default IndexPage;
